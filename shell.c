7f47190bbd08b7f22d04f906ed2caaa1e35f806a
mihn <martin.henrnav@gmail.com>
2026-08-30 01:33:46 -0400

    Agregar soporte para el comando 'cd' en la ejecución de comandos.
    Crear un enum para los operadores de control de flujo para desarrollo posterior puesto que no son operadores built-in

--- shell.c
+++ shell.c
@@ -7,8 +7,39 @@
 #define MAX_INPUT 1024
 #define MAX_ARGS 64
 
+// definir los operadores para la ejecucion de multiples comandos
+typedef enum {
+    OP_NONE,
+    OP_AND,
+    OP_OR,
+    OP_PIPE,
+    OP_SEMICOLON
+} Operador;
+
 int ejecutar_comando(char *args[]) {
     if (args[0] == NULL) return 0;
+
+    if (strcmp(args[0], "cd") == 0) {
+        const char *path;
+
+        if (args[1] == NULL || strcmp(args[1], "~") == 0) {
+            path = getenv("HOME");
+
+            if (path == NULL) {
+                fprintf(stderr, "cd: HOME no definido\n");
+                return EXIT_FAILURE;
+            }
+        } else {
+            path = args[1];
+        }
+
+        if (chdir(path) < 0) {
+            perror("cd");
+            return EXIT_FAILURE;
+        }
+
+        return EXIT_SUCCESS;
+    }
 
     // crear un proceso hijo para ejecutar el comando
     pid_t pid = fork();
